85b9cb9858129f76ced528e1eec3fecf2289efb6
Hui-Hong You <hiroshi@ghostsinthelab.org>
2021-01-26 23:20:34 +0800

    No more TOML, so rename the method to safe_key_format
    Signed-off-by: Hui-Hong You <hiroshi@ghostsinthelab.org>

--- data/emoji-listed-as-json.rb
+++ data/emoji-listed-as-json.rb
@@ -5,24 +5,24 @@ require 'oj'
 require 'unicode/emoji'
 
 class String
-  def safe_toml_key
-    safe_toml_key = self.dup
-    safe_toml_key.gsub!(" ", "_")
-    safe_toml_key.gsub!("&", "and")
-    safe_toml_key.downcase!
-    safe_toml_key
+  def safe_key_format
+    safe_key_format = self.dup
+    safe_key_format.gsub!(" ", "_")
+    safe_key_format.gsub!("&", "and")
+    safe_key_format.downcase!
+    safe_key_format
   end
 end
 
 emoji_list = {}
 Unicode::Emoji.list.keys.each do |l|
-    emoji_list[l.safe_toml_key] = {}
-    emoji_list[l.safe_toml_key]['category'] = l
+    emoji_list[l.safe_key_format] = {}
+    emoji_list[l.safe_key_format]['category'] = l
     Unicode::Emoji.list(l).keys.each do |subcategory|
-        emoji_list[l.safe_toml_key][subcategory] = {}
-        emoji_list[l.safe_toml_key][subcategory]['emojis'] = []
+        emoji_list[l.safe_key_format][subcategory] = {}
+        emoji_list[l.safe_key_format][subcategory]['emojis'] = []
         Unicode::Emoji.list(l, subcategory).each do |emoji|
-            emoji_list[l.safe_toml_key][subcategory]['emojis'] << emoji
+            emoji_list[l.safe_key_format][subcategory]['emojis'] << emoji
         end
     end
 end
